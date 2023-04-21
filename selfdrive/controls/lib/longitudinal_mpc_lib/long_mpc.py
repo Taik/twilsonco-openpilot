993f12e69a9e414760171afc95675b6efd79b47e
Tim Wilson <twilsonco@gmail.com>
2023-04-21 08:32:45 -0600

    lower COMFORT_BRAKE

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -55,7 +55,7 @@ T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
 MAX_ACCEL = 2.0
 T_FOLLOW = 1.45
-COMFORT_BRAKE = 2.5
+COMFORT_BRAKE = 2.2
 STOP_DISTANCE = 6.0
 
 def get_stopped_equivalence_factor(v_lead):
