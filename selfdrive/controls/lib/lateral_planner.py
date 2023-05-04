0fab4b325743d7d21ad45fd1136a1a85dcd02247
Tim Wilson <twilsonco@gmail.com>
2023-05-04 12:08:45 -0600

    try rockerrock's lat mpc tune

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -15,14 +15,14 @@ LaneChangeState = log.LateralPlan.LaneChangeState
 
 
 PATH_COST = 1.0
-LATERAL_MOTION_COST = 0.1
-LATERAL_ACCEL_COST = 0.0
-LATERAL_JERK_COST = 0.08
+LATERAL_MOTION_COST = 0.11
+LATERAL_ACCEL_COST = 0.5
+LATERAL_JERK_COST = 0.05
 # Extreme steering rate is unpleasant, even
 # when it does not cause bad jerk.
 # TODO this cost should be lowered when low
 # speed lateral control is stable on all cars
-STEERING_RATE_COST = 500.0
+STEERING_RATE_COST = 5.0
 
 
 class LateralPlanner:
