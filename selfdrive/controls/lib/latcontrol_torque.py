801b6d21e0ff71ae0f1be0a0663086cde683efd7
Tim Wilson <twilsonco@gmail.com>
2023-05-09 08:04:02 -0600

    more smoothing

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -38,7 +38,7 @@ class LatControlTorque(LatControl):
     self.use_steering_angle = self.torque_params.useSteeringAngle
     self.steering_angle_deadzone_deg = self.torque_params.steeringAngleDeadzoneDeg
     self.error_downscale = 4.0
-    self.error_scale_factor = FirstOrderFilter(1.0, 2.0, 0.01)
+    self.error_scale_factor = FirstOrderFilter(1.0, 2.5, 0.01)
     self.use_nn = CI.initialize_ff_nn(CP.carFingerprint)
     if self.use_nn:
       self.torque_from_nn = CI.get_ff_nn
