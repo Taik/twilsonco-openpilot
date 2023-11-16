f6d2aef80b2fa2821ac47e096a384e6e1184f8a0
Thinh Nguyen <tnguyenx91@gmail.com>
2023-11-15 19:01:24 -0800

    Disable update inactivity warning

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -32,8 +32,8 @@ FINALIZED = os.path.join(STAGING_ROOT, "finalized")
 
 OVERLAY_INIT = Path(os.path.join(BASEDIR, ".overlay_init"))
 
-DAYS_NO_CONNECTIVITY_MAX = 14     # do not allow to engage after this many days
-DAYS_NO_CONNECTIVITY_PROMPT = 10  # send an offroad prompt after this many days
+DAYS_NO_CONNECTIVITY_MAX = 36500     # do not allow to engage after this many days - disable connect warning
+DAYS_NO_CONNECTIVITY_PROMPT = 36500  # send an offroad prompt after this many days - disable connect warning
 
 class WaitTimeHelper:
   def __init__(self):
